010bb920bd853bd651c8ccfbf03b96ae66a72013
ijp74190 <ijp74190@ucmo.edu>
2024-10-13 20:47:22 -0500

    Adds code for writing to DB
    This commit is NOT working right now in lambda.

---
+++ backend/src/handlers/project_write.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.handler = async (event) => {
+  const requestBody = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.PROJECT_TABLE,
+    Key: {
+      projectID: requestBody.projectID,
+      projectName: requestBody.projectName,
+    }
+  };
+
+
+  try {
+    dynamoDb.put(params, function (err, data) {
+      if (err) {
+        return {
+          statusCode: 401,
+          body: JSON.stringify({ message: 'Request Error' }),
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+          }
+        };
+      }
+      else {
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Writing successful' }),
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+          }
+        };
+      }
+    });
+
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal server error' }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      }
+    };
+  }
+};
