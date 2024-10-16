f0b37288853d1f1936742b45eb100f79a828640a
Aaron Delgado <aaronsmd35@gmail.com>
2024-10-16 13:50:15 -0500

    Created a read function and updated Keys

--- backend/src/handlers/project_read.js
+++ backend/src/handlers/project_read.js
@@ -1,24 +1,39 @@
 // Load the AWS SDK for Node.js
 const AWS = require("aws-sdk");
 // Create DynamoDB document client
-const docClient = new AWS.DynamoDB.DocumentClient();
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = async (event) => {
     const requestBody = JSON.parse(event.body);
     const params = {
         TableName: process.env.PROJECT_TABLE,
         Key: {
-            projectId: requestBody.projectId,
-            projectName: requestBody.projectName
+            projectId: requestBody.projectId
         },
     };
 
-    docClient.get(params, function (err, data) {
-        if (err) {
-            console.log("Error", err);
-        } else {
-            console.log("Success", data.Item);
+    try{
+        await dynamoDB.get().promise();
+        return{
+            statusCode: 200,
+            console: log("Success", data.Item),
+            body: JSON.stringify({message: "Reading Successful"}),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true
+            }
+        };
+    }
+    catch (error) {
+        console.error('Error:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({message: 'Internal server error'}),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true
+            }
         }
-    });
+    }
 };
 
