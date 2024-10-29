397e29347c1ccf96d8626bd30d77dbba5b8d99f5
laurenschletzbaum <les48220@ucmo.edu>
2024-10-29 13:49:29 -0500

    working on button funct

--- frontend/src/app/Timesheet/timesheet.ts
+++ frontend/src/app/Timesheet/timesheet.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-timesheet',
@@ -8,6 +11,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 })
 
 export class TimesheetComponent {
+  [x: string]: any;
+
   employeeName: string = '';
   selectedMonth: string = '';
   months: string[] = [
@@ -27,13 +32,14 @@ export class TimesheetComponent {
     const daysInMonth = new Date(2024, this.months.indexOf(this.selectedMonth) + 1, 0).getDate();
 
     // Create an array of day numbers
-    this.days = Array.from({ length: daysInMonth }, (_, i) => `${i + 1}`);
+    this.days = Array.from({length: daysInMonth}, (_, i) => `${i + 1}`);
 
     // Reset the chart data to 0 for each day
     this.chartData = Array(daysInMonth).fill(0);
 
     this.updateTotalHours();
   }
+
 
   validateInput(event: any) {
     //  only numbers are entered in the input field
@@ -43,7 +49,44 @@ export class TimesheetComponent {
     }
     this.updateTotalHours();
   }
+
   updateTotalHours() {
     this.totalHours = this.chartData.reduce((sum, current) => sum + current, 0);
+  }
+
+  exportToExcel() {
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet([
+      ['Employee Name:', this.employeeName],
+      ['Month:', this.selectedMonth],
+      ['Days:', ...this.days],
+      ['Hours:', ...this.chartData],
+      ['Total Hours:', this.totalHours]
+    ]);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Timesheet');
+
+    const excelBuffer: any = XLSX.write(wb, {
+      bookType: 'xlsx',
+      type: 'array'
+    });
+
+        this['saveAsExcelFile'](excelBuffer, 'exported-data');
+  }
+
+
+  onSave() {
+    if (this['timesheet'].valid) {
+      // Get the form values
+      const formData = this['timesheet'].value;
+
+
+      this.saveData(formData);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  saveData(data: any) {
+    console.log('Saved data:', data);
   }
 }
