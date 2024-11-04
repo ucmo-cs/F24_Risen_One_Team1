4be6f85f38d96c5130946d1550dbd2e6b3ea0d70
Aaron Delgado <aaronsmd35@gmail.com>
2024-11-04 11:25:11 -0600

    Update function to update the time data item in dynmoDB

---
+++ backend/src/handlers/project_update.js
@@ -0,0 +1,31 @@
+// Load the AWS SDK for Node.js
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, UpdateCommand } = require("@aws-sdk/lib-dynamodb");
+
+// Create DynamoDB client
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+module.exports.handler = async (event) => {
+    const requestBody = JSON.parse(event.body);
+
+    const command = new UpdateCommand({
+        TableName: process.env.PROJECT_TABLE,
+        Key: {
+            projectId: requestBody.projectId
+        },
+        UpdateExpression: "set #yr.#mo[0].time[0] = :time",
+        ExpressionAttributeNames: {
+            "#yr": "year",
+            "#mo": "10"
+        },
+        ExpressionAttributeValues: {
+            ":time": 4
+        },
+        ReturnValues: "ALL_NEW"
+    });
+
+    const response = await docClient.send(command);
+    console.log(response);
+    return response;
+};
